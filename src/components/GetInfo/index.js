cc6b556186f2ecfbb3faa729f1427afa7a93b625
Mohana Bansal <mohanabansal10@gmail.com>
2020-10-06 08:44:24 -0400

    adds reducer

--- src/components/GetInfo/index.js
+++ src/components/GetInfo/index.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 function GetInfo() {
+
+  useEffect(() => {
+
+  }, [])
+
   return(
     <div>
       <h1>Welcom to the Test App</h1>
--- src/redux/GetInfo.js
+++ src/redux/GetInfo.js
@@ -1,13 +1,37 @@
 //action type
+const GET_INFO = "GET_INFO";
 
 //action creator
+const getInfo = (info) => ({
+  type: GET_INFO,
+  info,
+});
 
 //thunk
+export const getInfoFromAPI = () => {
+  return async (dispatch, getState, { axios }) => {
+    try {
+      const { data } = await axios.get(
+        "https://cors-anywhere.herokuapp.com/fetch-hiring.s3.amazonaws.com/hiring.json"
+      );
+      dispatch(getInfo(data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
 
-const initialState = {};
+const initialState = {
+  info: []
+};
 
 export default function getInfoReducer(state = initialState, action) {
   switch (action.type) {
+    case GET_INFO:
+      return {
+        ...state,
+        info: action.info
+      }
     default:
       return state;
   }
